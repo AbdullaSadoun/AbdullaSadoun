12aac81378ce9a3702e09691dc5cb37775575ea8
AbdullaSadoun <abdullasadoun99@gmail.com>
2024-12-29 07:18:02 -0800

    fixed mime type issue for deployment

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Use root path since we're using a custom domain
+  base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    sourcemap: false,
+    minify: 'terser',
+  },
 }) 
